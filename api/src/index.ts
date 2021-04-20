014717e2493a0281893e12df227ae08659251f1a
tkdn <tkdnation@gmail.com>
2021-04-21 05:48:23 +0900

    package: typedi 使ってないので削除

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,14 +3,12 @@ import express from "express";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import { ApolloServer } from "apollo-server-express";
-import { createConnection, useContainer } from "typeorm";
-import { Container } from "typedi";
 
 import { cacheClient } from "./infra/cache/cacheClient";
 import { Task } from "./infra/database/entities/Task";
 import { User } from "./infra/database/entities/User";
+import { createConnection } from "typeorm";
 
-useContainer(Container);
 
 const RedisStore = connectRedis(session);
 const sessionHandler = session({
